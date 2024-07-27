56eedb57ac41989ec0e257246889fa24711db314
Diegoalves122 <diegoalves1233455@gmail.com>
2024-07-27 12:07:31 -0300

    Add files via upload

---
+++ Calculos/src/Calculos.java
@@ -0,0 +1,12 @@
+import java.util.Scanner
+public class Calculos {
+
+    public static void Main(String [] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        double area;
+        double perimetro;
+        double 
+    }
+}
